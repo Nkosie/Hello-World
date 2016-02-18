f78e09bf7d9969353ea9471955d99d7e77f2b8b1
Nkoskhona <nk0skh0n2@live.com>
2016-02-18 21:50:56 +0200

    check1

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 int main()
 {
+	//just checking nje!!!
+
 	cout << "Hello, Nkosie" << endl;
 	cout << "today is wednesday!" << endl;
+	cout << "How cool is programming? :) Rate it out of 10 :O " << endl;
 
 }
